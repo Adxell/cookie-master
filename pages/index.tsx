a66c87e4df49abf58058a74d5eb255a1ecbcceb0
adxell-arango-clickonplans <90437102+Adxell@users.noreply.github.com>
2022-10-05 16:14:17 -0500

    updated pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Adxell Next!</a>
+          Welcome to <a href="https://nextjs.org">Adxell Arango Next!</a>
         </h1>
 
         <p className={styles.description}>
