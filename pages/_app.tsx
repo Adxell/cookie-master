5a87ed1b956ae949970d5436b3a3d40925d36771
adxell-arango-clickonplans <90437102+Adxell@users.noreply.github.com>
2022-10-06 17:09:20 -0500

    updated state compoment _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,25 @@ import '../styles/globals.css'
 import type { AppContext, AppProps } from 'next/app'
 import { CssBaseline, Theme, ThemeProvider } from '@mui/material'
 import { darkTheme, customTheme, lightTheme} from '../themes'
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 
 interface Props extends AppProps {
   theme: string;
 }
 
 function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
-
-  const currentTheme:Theme = theme === 'light'
+  const [currentTheme, setCurrentTheme] = useState(lightTheme)
+  
+  useEffect(() => {
+    const cookieTheme = Cookies.get('theme') || 'light'
+    const selectedTheme :Theme = cookieTheme === 'light'
       ? lightTheme
-      : (theme === 'dark')
+      : (cookieTheme === 'dark')
         ? darkTheme
         : customTheme
-
+    setCurrentTheme(selectedTheme)
+  }, [])
 
   return (
     <ThemeProvider theme={currentTheme}>
