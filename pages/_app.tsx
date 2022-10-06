68cc25770e39db32b34bf44229260ccafa4b9f25
adxell-arango-clickonplans <90437102+Adxell@users.noreply.github.com>
2022-10-06 16:53:19 -0500

    updated _aap.tsx, changing color theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,38 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { darkTheme } from '../themes'
+import type { AppContext, AppProps } from 'next/app'
+import { CssBaseline, Theme, ThemeProvider } from '@mui/material'
+import { darkTheme, customTheme, lightTheme} from '../themes'
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface Props extends AppProps {
+  theme: string;
+}
+
+function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
+
+  const currentTheme:Theme = theme === 'light'
+      ? lightTheme
+      : (theme === 'dark')
+        ? darkTheme
+        : customTheme
+
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={currentTheme}>
       <CssBaseline/>
       <Component {...pageProps} />
     </ThemeProvider>
   )
 }
+
+// MyApp.getInitialProps = async( appContext: AppContext ) => {
+//   const { theme } = appContext.ctx.req ? (appContext.ctx.req as any).cookies : { theme: 'light' }
+  
+//   const validTheme = ['light', 'dark', 'custom']
+
+
+//   return {
+//       theme: validTheme.includes( theme ) ? theme : 'dark'
+//   }
+// }
 
 export default MyApp
